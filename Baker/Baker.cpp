3ccee4dfc79fbd1111c3bef9b84260602381914f
Volkov Daniil <volkov.study@mail.ru>
2022-10-13 20:16:52 +0300

    Add Order's field to Baker class

--- Baker/Baker.cpp
+++ Baker/Baker.cpp
@@ -5,3 +5,11 @@ Baker::Baker(int workExperience) : workExperience(workExperience) {}
 int Baker::getWorkExperience() const {
     return workExperience;
 }
+
+Order *Baker::getOrder() const {
+    return order;
+}
+
+void Baker::setOrder(Order *order) {
+    Baker::order = order;
+}
--- Baker/Baker.h
+++ Baker/Baker.h
@@ -1,14 +1,19 @@
 #ifndef BAKERY_BUSINESS_BAKER_H
 #define BAKERY_BUSINESS_BAKER_H
 
+#include "../Order/Order.h"
 
 class Baker {
 private:
     int workExperience;
+    Order* order;
 public:
     explicit Baker(int workExperience);
 
     int getWorkExperience() const;
+    Order *getOrder() const;
+
+    void setOrder(Order *order);
 };
 
 
