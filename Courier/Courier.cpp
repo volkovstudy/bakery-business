76dc71aeb259b2130d0061cc21b169028d09a98f
Volkov Daniil <volkov.study@mail.ru>
2022-10-13 20:14:50 +0300

    Add Order's field to Courier class

--- Courier/Courier.cpp
+++ Courier/Courier.cpp
@@ -5,3 +5,11 @@ Courier::Courier(int trunkVolume) : trunkVolume(trunkVolume) {}
 int Courier::getTrunkVolume() const {
     return trunkVolume;
 }
+
+Order *Courier::getOrder() const {
+    return order;
+}
+
+void Courier::setOrder(Order *order) {
+    Courier::order = order;
+}
--- Courier/Courier.h
+++ Courier/Courier.h
@@ -1,14 +1,19 @@
 #ifndef BAKERY_BUSINESS_COURIER_H
 #define BAKERY_BUSINESS_COURIER_H
 
+#include "../Order/Order.h"
 
 class Courier {
 private:
     int trunkVolume;
+    Order* order;
 public:
     explicit Courier(int trunkVolume);
 
     int getTrunkVolume() const;
+    Order *getOrder() const;
+
+    void setOrder(Order *order);
 };
 
 
