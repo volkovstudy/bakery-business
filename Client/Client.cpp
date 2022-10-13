c7b576fd27aa33f1a50249273372970b2ae4046f
Volkov Daniil <volkov.study@mail.ru>
2022-10-13 20:12:17 +0300

    Add vector of Order in Client class

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,7 +1,11 @@
 #include "Client.h"
 
-Client::Client(const string &fullName) : fullName(fullName) {}
+Client::Client(const string &fullName, const vector<Order> &orders) : fullName(fullName), orders(orders) {}
 
 const string &Client::getFullName() const {
     return fullName;
+}
+
+const vector<Order> &Client::getOrders() const {
+    return orders;
 }
--- Client/Client.h
+++ Client/Client.h
@@ -2,16 +2,20 @@
 #define BAKERY_BUSINESS_CLIENT_H
 
 #include <string>
+#include <vector>
+#include "../Order/Order.h"
 
 using namespace std;
 
 class Client {
 private:
     string fullName;
+    vector<Order> orders;
 public:
-    explicit Client(const string &fullName);
+    Client(const string &fullName, const vector<Order> &orders);
 
     const string &getFullName() const;
+    const vector<Order> &getOrders() const;
 };
 
 
