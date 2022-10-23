8f9a30e5379b4a3d7c2630634322cbdf1e64d0c1
Volkov Daniil <volkov.study@mail.ru>
2022-10-23 18:38:07 +0300

    Add realization for OrderProcessing#cookOrder(...)

--- OrderProcessing/OrderProcessing.cpp
+++ OrderProcessing/OrderProcessing.cpp
@@ -1,3 +1,27 @@
 #include "OrderProcessing.h"
 
+#define startIdForBaker -1
+
 OrderProcessing::OrderProcessing(Staff *staff, Storage *storage) : staff(staff), storage(storage) {}
+
+int nextBakerIndex = startIdForBaker;
+
+Baker* getNextBaker(vector<Baker*> bakers) {
+    if (bakers.size() <= nextBakerIndex) {
+        nextBakerIndex = 0;
+    } else {
+        nextBakerIndex++;
+    }
+
+    return bakers.at(nextBakerIndex);
+}
+
+void OrderProcessing::cookOrder(Order *order) {
+    if (order->getStatus() != Status::ACCEPTED) return;
+
+    vector<Baker*> bakers = staff->getBakers();
+    Baker* baker = getNextBaker(bakers);
+
+    baker->setOrder(order);
+    baker->cook();
+}
