651da176eaf58673c3c5a9b0c9c54a369de2933f
Volkov Daniil <volkov.study@mail.ru>
2022-10-23 18:05:41 +0300

    Create OrderProcessing class without realization

---
+++ OrderProcessing/OrderProcessing.cpp
@@ -0,0 +1,1 @@
+#include "OrderProcessing.h"
---
+++ OrderProcessing/OrderProcessing.h
@@ -0,0 +1,13 @@
+#ifndef BAKERY_BUSINESS_ORDERPROCESSING_H
+#define BAKERY_BUSINESS_ORDERPROCESSING_H
+
+
+class OrderProcessing {
+public:
+    void assignOrder();
+    void storeOrder();
+    void deliverOder();
+};
+
+
+#endif
